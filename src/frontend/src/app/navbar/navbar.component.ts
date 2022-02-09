75f8f30e87d2a9d908312a1fabd4ea959869c753
swansobp <swansobp@mail.uc.edu>
2022-02-08 21:00:49 -0500

    Added Dark Mode
    Added light/dark mode, fixed routing error, and added theme to browser local storage.

--- src/frontend/src/app/navbar/navbar.component.ts
+++ src/frontend/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
+import { BoundDirectivePropertyAst } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  darkButton!: HTMLElement;
+  lightButton!: HTMLElement;
+  unorderedList: HTMLElement;
+  body!: HTMLElement;
+
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
+    const darkButton = document.getElementById('dark');
+    const lightButton = document.getElementById('light');
+    const unorderedList = document.getElementById('unorderedList');
+    const body = document.body;
+
+    // Apply cached theme on reload
+    const theme = localStorage.getItem('theme');
+
+    if (theme) {
+      body.classList.add(theme);
+      unorderedList.classList.add(theme);
+      //console.log("");
+    }
+
+    darkButton.onclick = () => {
+      body.classList.replace('light', 'dark');
+      unorderedList.classList.replace('light', 'dark');
+      localStorage.setItem('theme', 'dark');
+    }
+
+    lightButton.onclick = () => {
+      body.classList.replace('dark', 'light');
+      unorderedList.classList.replace('dark', 'light');
+      localStorage.setItem('theme', 'light');
+    }
   }
+
+  
 
 }
