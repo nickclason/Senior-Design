be1820cb43dedb96ab11d4ab5bd78dd17bddb40b
Nick Clason <nick.clason1@gmail.com>
2022-03-06 21:11:35 -0500

    Add example event listener
    Idk how possible this is, but i'm exhausted and can't think clearly about this anymore.

--- src/frontend/src/app/dashboard/dashboard.component.ts
+++ src/frontend/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-
+import { ResizeEventArgs } from '@syncfusion/ej2-angular-layouts';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,4 +10,16 @@ import { Component, ViewEncapsulation } from '@angular/core';
 export class DashboardComponent {
   public cellSpacing: number[] = [10, 10];
   public mediaQuery: string = 'max-width: 700px';
+
+
+  private eventTrace: string = '';
+
+  public afterResize(args: ResizeEventArgs): void {
+    this.updateEventLog(args);
+    console.log(this.eventTrace);
+  }
+
+  private updateEventLog(args: any): void {
+    this.eventTrace = this.eventTrace + args.name + ' Event triggered. <br />'
+}
 }
