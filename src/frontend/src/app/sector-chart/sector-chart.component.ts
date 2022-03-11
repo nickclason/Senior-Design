8504dfcbd9ee37931ceaf6ac68b6c4b65cbeda02
Nick Clason <nick.clason1@gmail.com>
2022-03-10 22:29:32 -0500

    Fix issue with hard-coded sector-map values for coloring

--- src/frontend/src/app/sector-chart/sector-chart.component.ts
+++ src/frontend/src/app/sector-chart/sector-chart.component.ts
@@ -44,9 +44,39 @@ export class SectorChartComponent implements OnInit {
   ngOnInit(): void {
     this.dataService.getSectorChartData().subscribe(data => {
       this.data = data;
+
+      let min = this.data[0].value;
+      let max = min;
+      for (let i = 0; i < this.data.length; i++) {
+        if (this.data[i].value > max) {
+          max = this.data[i].value;
+        }
+        if (this.data[i].value < min) {
+          min = this.data[i].value;
+        }
+      }
+
+      
+      this.leafItemSettings = {
+        labelPath: 'sector',
+        colorMapping: [
+          {
+            from: min,
+            to: max,
+            minOpacity: 0.1,
+            maxOpacity: 1.0,
+            color: '#b4256c',
+          }],
+        labelStyle: {
+          color: 'black', //  text color
+        }
+
+      };
     });
 
     this.dataService.loadSectorChartData();
   }
 
 }
+
+
