3334232666f1d5560b79bf1e1246b4d3e03c515a
Nick Clason <nick.clason1@gmail.com>
2022-03-21 10:37:11 -0400

    Add date-picker to House-Info and Senate-Info

--- src/frontend/src/app/house-info/house-info.component.ts
+++ src/frontend/src/app/house-info/house-info.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { StockInfoComponent } from '../stock-info/stock-info.component';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-house-info',
@@ -14,13 +15,15 @@ export class HouseInfoComponent implements OnInit {
   hasTransactions: boolean = true;
   transactions: any;
   displayedColumns: string[] = ['name', 'district', 'ticker', 'amount', 'filing_date', 'transaction_date', 'transaction_type', 'owner'];
-
+  dateForm = new FormGroup({
+    date: new FormControl(new Date(), Validators.required)
+  });
 
   constructor(private http: HttpClient, private dialog: MatDialog) { }
 
   ngOnInit(): void {
 
-    this.http.get<any>('http://localhost:5000/data/get_house_info?date=03_07_2022').subscribe(data => {
+    this.http.get<any>('http://localhost:5000/data/get_house_info').subscribe(data => {
       let resp = data['data'];
       if (resp == -1) {
         this.hasTransactions = false;
@@ -45,5 +48,30 @@ export class HouseInfoComponent implements OnInit {
       console.log('The stock dialog was closed');
     });
   }
+
+  selectDate() {
+    
+    let date = this.dateForm.value.date;
+    console.log(date)
+
+    var dd = String(date.getDate()).padStart(2, '0');
+    var mm = String(date.getMonth() + 1).padStart(2, '0');
+    var yyyy = date.getFullYear();
+
+    const d = mm + '_' + dd + '_' + yyyy;
+    console.log(d);
+
+    this.http.get<any>('http://localhost:5000/data/get_house_info?date='+d).subscribe(data => {
+      let resp = data['data'];
+      if (resp == -1) {
+        this.hasTransactions = false;
+        return;
+      }
+
+      this.hasTransactions = true;
+      this.transactions = resp;
+    });
+  }
+  
 
 }
--- src/frontend/src/app/senate-info/senate-info.component.ts
+++ src/frontend/src/app/senate-info/senate-info.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { StockInfoComponent } from '../stock-info/stock-info.component';
-
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 @Component({
   selector: 'app-senate-info',
   templateUrl: './senate-info.component.html',
@@ -15,11 +15,15 @@ export class SenateInfoComponent implements OnInit {
   transactions: any;
   displayedColumns: string[] = ['senator', 'ticker', 'asset_type', 'amount', 'disclosure_date', 'transaction_date', 'transaction_type', 'owner'];
 
+  dateForm = new FormGroup({
+    date: new FormControl(new Date(), Validators.required)
+  });
+
   constructor(private http: HttpClient, private dialog: MatDialog) { }
 
   ngOnInit(): void {
 
-    this.http.get<any>('http://localhost:5000/data/get_senate_info?date=02_14_2022').subscribe(data => {
+    this.http.get<any>('http://localhost:5000/data/get_senate_info').subscribe(data => {
       let resp = data['data'];
       if (resp == -1) {
         this.hasTransactions = false;
@@ -44,6 +48,31 @@ export class SenateInfoComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       // console.log('The stock dialog was closed');
+    });
+  }
+
+  selectDate() {
+    let date = this.dateForm.value.date;
+    console.log(date)
+
+    var dd = String(date.getDate()).padStart(2, '0');
+    var mm = String(date.getMonth() + 1).padStart(2, '0');
+    var yyyy = date.getFullYear();
+
+    const d = mm + '_' + dd + '_' + yyyy;
+    console.log(d);
+
+    this.http.get<any>('http://localhost:5000/data/get_senate_info?date='+d).subscribe(data => {
+      let resp = data['data'];
+      if (resp == -1) {
+        this.hasTransactions = false;
+        return;
+      }
+
+      this.hasTransactions = true;
+      this.transactions = resp;
+      console.log(this.transactions);
+
     });
   }
 
